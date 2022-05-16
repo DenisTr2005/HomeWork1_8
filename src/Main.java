1460c9ac2d986ceb68919d91af073b2aa36ed59c
Денис <den_2005@list.ru>
2022-05-16 12:21:15 +0300

    Second commit

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,10 @@
 import java.time.LocalDate;
-import java.util.Scanner;
+import java.util.Arrays;
 
 public class Main {
 
     public static void main(String[] args) {
-        Scanner in = new Scanner(System.in);
+/*        Scanner in = new Scanner(System.in);
 //Задание 1
         System.out.print("Введите год: ");
         checkYearLeap(in.nextInt());
@@ -16,6 +16,16 @@ public class Main {
 //Задание 3
         System.out.print("Введите дистанцию до клиента в км: ");
         System.out.println("Потребуется дней: " + calcDeliveryDays(in.nextInt()));
+*/ //Задание 4
+        int[] arr4 = {3, 2, 1, 6, 5};
+        toReverseArr(arr4);
+        System.out.println(Arrays.toString(arr4));
+//Задание 5
+        String string5 = "aabccddefgghiijjkk";
+        duplicate(string5);
+//Задание 6
+        int[] arr6 = generateRandomArray();
+        System.out.printf("Средняя сумма трат за месяц составила %.2f рублей", average(arr6));
     }
     public static void checkYearLeap(int yearCheck) {
         boolean isYearLeap = (yearCheck % 4 == 0 && yearCheck % 100 != 0) || yearCheck % 400 == 0;
@@ -48,5 +58,44 @@ public class Main {
             day += 3;
         }
         return day;
+    }
+    public static void toReverseArr(int[] arr) {
+        for (int i = 0; i < arr.length/2; i++) {
+            int buffer = arr[i];
+            arr[i] = arr[arr.length - i - 1];
+            arr[arr.length - i - 1] = buffer;
+        }
+    }
+    public static void duplicate(String string) {
+        for (int i = 1; i < string.length(); i++) {
+            if (string.charAt(i) == string.charAt(i - 1)) {
+                System.out.println("В строке присутствует задублированный символ " + string.charAt(i));
+                return;
+            }
+        }
+        System.out.println("В строке нет дублей");
+    }
+
+    public static double average(int[] arr) {
+        return toDouble(sum(arr)) / arr.length;
+    }
+    private static double toDouble(int integer) {
+        return integer;
+    }
+
+    private static int sum(int[] arr) {
+        int sum = 0;
+        for (int i : arr) {
+            sum += i;
+        }
+        return sum;
+    }
+    public static int[] generateRandomArray() {
+        java.util.Random random = new java.util.Random();
+        int[] arr = new int[30];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = random.nextInt(100_000) + 100_000;
+        }
+        return arr;
     }
 }
